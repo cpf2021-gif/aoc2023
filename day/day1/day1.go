6e1188b4211ed22523edc7f30c16b2656c14ded4
xypf <2992247892@qq.com>
2023-12-06 19:12:15 +0800

    fmt day1

--- day/day1/day1.go
+++ day/day1/day1.go
@@ -8,26 +8,27 @@ import (
 
 var scoreMap = map[string]int{
 	"one":   1,
-	"1": 1,
+	"1":     1,
 	"two":   2,
-	"2": 2,
+	"2":     2,
 	"three": 3,
-	"3": 3,
+	"3":     3,
 	"four":  4,
-	"4": 4,
+	"4":     4,
 	"five":  5,
-	"5": 5,
+	"5":     5,
 	"six":   6,
-	"6": 6,
+	"6":     6,
 	"seven": 7,
-	"7": 7,
+	"7":     7,
 	"eight": 8,
-	"8": 8,
+	"8":     8,
 	"nine":  9,
-	"9": 9,
+	"9":     9,
 }
 
 type mode int
+
 const (
 	TestMode mode = iota
 	DevMode
@@ -69,22 +70,21 @@ func (d *Day1) Q1() int64 {
 		d.Data = util.ReadByLine("./data/day1_q1_test.txt")
 	} else {
 		d.Data = util.ReadByLine("./data/day1_input.txt")
-	} 
+	}
 
 	var res int64 = 0
 
 	for _, v := range d.Data {
 		var lnum int = 0
 		var rnum int = 0
-		
+
 		str, _ := findFirstMatch(v, "1|2|3|4|5|6|7|8|9")
 		lnum = scoreMap[str]
-
 
 		str, _ = findLastMatch(v, "1|2|3|4|5|6|7|8|9")
 		rnum = scoreMap[str]
 
-		res += int64(lnum * 10 + rnum)
+		res += int64(lnum*10 + rnum)
 	}
 
 	return res
@@ -95,21 +95,21 @@ func (d *Day1) Q2() int64 {
 		d.Data = util.ReadByLine("./data/day1_q2_test.txt")
 	} else {
 		d.Data = util.ReadByLine("./data/day1_input.txt")
-	} 
+	}
 
 	var res int64 = 0
 
 	for _, v := range d.Data {
 		var lnum int = 0
 		var rnum int = 0
-		
+
 		str, _ := findFirstMatch(v, "1|2|3|4|5|6|7|8|9|one|two|three|four|five|six|seven|eight|nine")
 		lnum = scoreMap[str]
 
 		str, _ = findFirstMatch(util.ReverseSrting(v), "1|2|3|4|5|6|7|8|9|eno|owt|eerht|ruof|evif|xis|neves|thgie|enin")
 		rnum = scoreMap[util.ReverseSrting(str)]
 
-		res += int64(lnum * 10 + rnum)
+		res += int64(lnum*10 + rnum)
 	}
 
 	return res
--- day/day1/day1_test.go
+++ day/day1/day1_test.go
@@ -9,7 +9,6 @@ func TestQ1(t *testing.T) {
 	var have int64 = day1.Q1()
 	var want int64 = 142
 
-
 	if want != have {
 		t.Fatalf("Expected %d got %d", want, have)
 	}
@@ -19,7 +18,6 @@ func TestQ2(t *testing.T) {
 	day1 := NewDay1(TestMode)
 	var have int64 = day1.Q2()
 	var want int64 = 281
-
 
 	if want != have {
 		t.Fatalf("Expected %d got %d", want, have)
